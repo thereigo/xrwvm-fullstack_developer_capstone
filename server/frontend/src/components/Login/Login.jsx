a71a8d1ac470754c5ee257f76acc2b86e4ff4ce5
thereigo <ig.pozdnakovs@gmail.com>
2024-05-14 11:00:24 -0400

    Second module items added

--- server/frontend/src/components/Login/Login.jsx
+++ server/frontend/src/components/Login/Login.jsx
@@ -51,19 +51,22 @@ const Login = ({ onClose }) => {
         className='modalContainer'
       >
           <form className="login_panel" style={{}} onSubmit={login}>
-              <div>
+              <div className='input-form'>
               <span className="input_field">Username </span>
               <input type="text"  name="username" placeholder="Username" className="input_field" onChange={(e) => setUserName(e.target.value)}/>
               </div>
-              <div>
+              <div className='input-form'>
               <span className="input_field">Password </span>
               <input name="psw" type="password"  placeholder="Password" className="input_field" onChange={(e) => setPassword(e.target.value)}/>            
               </div>
-              <div>
+              <div className='form-buttons'>
               <input className="action_button" type="submit" value="Login"/>
               <input className="action_button" type="button" value="Cancel" onClick={()=>setOpen(false)}/>
               </div>
-              <a className="loginlink" href="/register">Register Now</a>
+              <p className='register-text'>
+                Don't have an account? <a className="register-link" href="/register">Register Now</a>
+              </p>
+              
           </form>
       </div>
     </div>
