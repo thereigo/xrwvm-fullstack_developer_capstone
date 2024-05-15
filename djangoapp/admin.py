b19762216ce36f70829c03d99bb2ce7420f0fb7e
“thereigo” <ig.pozdnakovs@gmail.com>
2024-05-15 03:53:10 -0400

    3rd module carmodel and carmake creation

---
+++ djangoapp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import CarMake, CarModel
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+
+
+# CarModelInline class
+
+# CarModelAdmin class
+
+# CarMakeAdmin class with CarModelInline
+
+# Register models here
---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+
+import "./Login.css";
+import Header from '../Header/Header';
+
+const Login = ({ onClose }) => {
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [open,setOpen] = useState(true)
+
+  let login_url = window.location.origin+"/djangoapp/login";
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch(login_url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "userName": userName,
+            "password": password
+        }),
+    });
+    
+    const json = await res.json();
+    if (json.status != null && json.status === "Authenticated") {
+        sessionStorage.setItem('username', json.userName);
+        setOpen(false);        
+    }
+    else {
+      alert("The user could not be authenticated.")
+    }
+};
+
+  if (!open) {
+    window.location.href = "/";
+  };
+  
+
+  return (
+    <div>
+      <Header/>
+    <div onClick={onClose}>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className='modalContainer'
+      >
+          <form className="login_panel" style={{}} onSubmit={login}>
+              <div className='input-form'>
+              <span className="input_field">Username </span>
+              <input type="text"  name="username" placeholder="Username" className="input_field" onChange={(e) => setUserName(e.target.value)}/>
+              </div>
+              <div className='input-form'>
+              <span className="input_field">Password </span>
+              <input name="psw" type="password"  placeholder="Password" className="input_field" onChange={(e) => setPassword(e.target.value)}/>            
+              </div>
+              <div className='form-buttons'>
+              <input className="action_button" type="submit" value="Login"/>
+              <input className="action_button" type="button" value="Cancel" onClick={()=>setOpen(false)}/>
+              </div>
+              <p className='register-text'>
+                Don't have an account? <a className="register-link" href="/register">Register Now</a>
+              </p>
+              
+          </form>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Login;
